cc0d62dc92d5ae9d0d210c2c19d7e627543a1123
Vitor <vitor.nunes@contmatic.com.br>
2020-07-01 11:41:23 -0300

    Commit Prova-03

--- src/main/java/br/com/contmatic/contato/Contato.java
+++ src/main/java/br/com/contmatic/contato/Contato.java
@@ -1,13 +1,20 @@
 package br.com.contmatic.contato;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 public class Contato {
     
+	@Pattern(regexp = "\\d{8}", message = "O telefone deve conter somente 8 digítos numéricos")
     private String telefone;
     
-    private String celular;
+	@Pattern(regexp = "\\d{9}", message = "O celular deve conter somente 9 digítos numéricos")
+	private String celular;
     
+	@Pattern(regexp = "\\d{8,9}", message = "O número para contato deve conter somente 8 ou 9 digítos numéricos")
     private String recado;
     
+    @Email(message = "O endereço de email é inválido")
     private String email;
     
     public Contato(String telefone, String celular, String recado, String email) {
@@ -22,42 +29,41 @@ public class Contato {
     }
 
     public void setTelefone(String telefone) {
-        if(telefone != null) {
-        verificaTamanhoTelefone(telefone);
-        verificaContemSomenteNumeros(telefone);
-        }
+//        if(telefone != null) {
+//        verificaTamanhoTelefone(telefone);
+//        verificaContemSomenteNumeros(telefone);
+//        }
+    	this.telefone = telefone;
     }
     
-    private void verificaContemSomenteNumeros(String campo) {
-        for(char palavra: campo.toCharArray()) {
-            if(!Character.isDigit(palavra)) {
-                throw new IllegalArgumentException("Só deve conter números");
-            }
-        }
-    }
-
-    private void verificaTamanhoTelefone(String telefone) {
-        if(telefone.length() != 8) {
-           throw new IllegalArgumentException("O número deve conter 8 dígitos"); 
-        }
-        this.telefone = telefone;
-    }
-
+//    private void verificaContemSomenteNumeros(String campo) {
+//        for(char palavra: campo.toCharArray()) {
+//            if(!Character.isDigit(palavra)) {
+//                throw new IllegalArgumentException("Só deve conter números");
+//            }
+//        }
+//    }
+//
+//    private void verificaTamanhoTelefone(String telefone) {
+//        if(telefone.length() != 8) {
+//           throw new IllegalArgumentException("O número deve conter 8 dígitos"); 
+//        }
+        
     public String getCelular() {
         return celular;
     }
 
     public void setCelular(String celular) {
-        if(celular != null) {
-        verificaTamanhoCelular(celular);
-        verificaContemSomenteNumeros(celular);
-        }
-    }
-
-    private void verificaTamanhoCelular(String celular) {
-        if(celular.length() != 9) {
-            throw new IllegalArgumentException("O número deve conter 9 dígitos"); 
-         }
+//        if(celular != null) {
+//        verificaTamanhoCelular(celular);
+//        verificaContemSomenteNumeros(celular);
+//        }
+//    }
+//
+//    private void verificaTamanhoCelular(String celular) {
+//        if(celular.length() != 9) {
+//            throw new IllegalArgumentException("O número deve conter 9 dígitos"); 
+//         }
         this.celular = celular;
     }
 
@@ -66,16 +72,16 @@ public class Contato {
     }
 
     public void setRecado(String recado) {
-        if(recado != null) {
-        verificaTamanhoRecado(recado);
-        verificaContemSomenteNumeros(recado);
-        }
-    }
-
-    private void verificaTamanhoRecado(String recado) {
-        if(recado.length() < 8 || recado.length() > 9) {
-            throw new IllegalArgumentException("O número deve conter 8 ou 9 dígitos"); 
-         }
+//        if(recado != null) {
+//        verificaTamanhoRecado(recado);
+//        verificaContemSomenteNumeros(recado);
+//        }
+//    }
+//
+//    private void verificaTamanhoRecado(String recado) {
+//        if(recado.length() < 8 || recado.length() > 9) {
+//            throw new IllegalArgumentException("O número deve conter 8 ou 9 dígitos"); 
+//         }
         this.recado = recado;
     }
 
@@ -84,20 +90,20 @@ public class Contato {
     }
 
     public void setEmail(String email) {
-        if(email != null) {
-        verificaTamanhoEmail(email);
-        }
-    }
-
-    private void verificaTamanhoEmail(String email) {
-        if(email.length() > 250) {
-            throw new IllegalArgumentException("O email deve ter até 100 caracteres");
-        }
-        for(char palavra: email.toCharArray()) {
-            if(Character.isSpaceChar(palavra)) {
-                throw new IllegalArgumentException("Não deve ter espaço");
-            }
-        }
+//        if(email != null) {
+//        verificaTamanhoEmail(email);
+//        }
+//    }
+//
+//    private void verificaTamanhoEmail(String email) {
+//        if(email.length() > 250) {
+//            throw new IllegalArgumentException("O email deve ter até 100 caracteres");
+//        }
+//        for(char palavra: email.toCharArray()) {
+//            if(Character.isSpaceChar(palavra)) {
+//                throw new IllegalArgumentException("Não deve ter espaço");
+//            }
+//        }
         this.email = email;
     }
     
--- src/main/java/br/com/contmatic/empresa/Cliente.java
+++ src/main/java/br/com/contmatic/empresa/Cliente.java
@@ -2,22 +2,45 @@ package br.com.contmatic.empresa;
 
 import java.time.LocalDate;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.contmatic.annotation.DataApos1900;
 import br.com.contmatic.contato.Contato;
 import br.com.contmatic.endereco.Endereco;
-import br.com.contmatic.validacao.ValidaCpf;
+//import br.com.contmatic.validacao.ValidaCpf;
 
 public class Cliente {
-    
+	
+	@Pattern(regexp = "[0-9]{1,5}", message = "Código inválido")
+    @NotEmpty(message = "Não deve aceitar código nulo nem vazio")
     private String codigo;
     
+	@Pattern(regexp = "^[A-ZÀ-Úa-zà-ú][A-ZÀ-Ú a-zà-ú]{0,98}[A-Za-zA-ZÀ-Úa-zà-ú]$", message = "Nome inválido")
+    @NotBlank(message = "Não deve aceitar nome nulo nem vazio")
     private String nome;
     
+	@Past(message = "A data de nascimento deve ser anterior a data atual")
+	@NotNull(message = "A data de nascimento não deve ser nula")
+	@DataApos1900
     private LocalDate dataNascimento;
     
+	@CPF
+    @NotNull(message = "Não deve aceitar cpf nulo")
     private String cpf;
     
+	@Valid
+    @NotNull(message = "Não deve aceitar endereco nulo")
     private Endereco endereco;
     
+	@Valid
+    @NotNull(message = "Não deve aceitar contato nulo")
     private Contato contato;
     
     public Cliente(String codigo, String nome, LocalDate dataNascimento, String cpf, Endereco endereco, Contato contato) {
@@ -34,148 +57,148 @@ public class Cliente {
     }
 
     public void setCodigo(String codigo) {
-        verificaCampoNulo(codigo);
-        verificaTamanhoMenorQue6(codigo);
-        verificaContemSomenteNumeros(codigo);
-        verificaCampoVazio(codigo);
+//        verificaCampoNulo(codigo);
+//        verificaTamanhoMenorQue6(codigo);
+//        verificaContemSomenteNumeros(codigo);
+//        verificaCampoVazio(codigo);
         this.codigo = codigo;
     }
     
-    private void verificaTamanhoMenorQue6(String campo) {
-        if(campo.length() > 5) {
-            throw new IllegalArgumentException("Este campo deve ter no máximo 5 dígitos");
-        }
-    }
-    
-    private void verificaCampoVazio(String campo) {
-        if(campo.isEmpty()) {
-            throw new IllegalArgumentException("Este campo não deve ser vazio");
-        }
-    }
-    
-    private void verificaContemSomenteNumeros(String campo) {
-        for(char palavra: campo.toCharArray()) {
-            if(!Character.isDigit(palavra)) {
-                throw new IllegalArgumentException("Só deve conter números");
-            }
-        }
-    }
+//    private void verificaTamanhoMenorQue6(String campo) {
+//        if(campo.length() > 5) {
+//            throw new IllegalArgumentException("Este campo deve ter no máximo 5 dígitos");
+//        }
+//    }
+//    
+//    private void verificaCampoVazio(String campo) {
+//        if(campo.isEmpty()) {
+//            throw new IllegalArgumentException("Este campo não deve ser vazio");
+//        }
+//    }
+//    
+//    private void verificaContemSomenteNumeros(String campo) {
+//        for(char palavra: campo.toCharArray()) {
+//            if(!Character.isDigit(palavra)) {
+//                throw new IllegalArgumentException("Só deve conter números");
+//            }
+//        }
+//    }
 
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
-        verificaCampoNulo(nome);
-        verificaCampoVazio(nome);
-        verificaEspacoInicio(nome);
-        verificaContemSomenteLetras(nome);
-        verificaTamanhoAteCem(nome);
+//        verificaCampoNulo(nome);
+//        verificaCampoVazio(nome);
+//        verificaEspacoInicio(nome);
+//        verificaContemSomenteLetras(nome);
+//        verificaTamanhoAteCem(nome);
         this.nome = nome;
     }
     
-    private void verificaEspacoInicio(String campo) {
-        if(campo.charAt(0) == ' ') {
-            throw new IllegalArgumentException("O campo não deve começar com espaços");
-        }
-    }
-    
-    private void verificaContemSomenteLetras(String campo) {
-        for(char palavra: campo.toCharArray()) {
-            if(!(Character.isAlphabetic(palavra) || Character.isSpaceChar(palavra))) {
-                throw new IllegalArgumentException("Só deve conter letras");
-            }
-        }
-    }
-
-    private void verificaTamanhoAteCem(String campo) {
-        if(campo.length() > 100) {
-        throw new IllegalArgumentException("O campo deve ter até 100 caracteres");
-        }
-    }
+//    private void verificaEspacoInicio(String campo) {
+//        if(campo.charAt(0) == ' ') {
+//            throw new IllegalArgumentException("O campo não deve começar com espaços");
+//        }
+//    }
+//    
+//    private void verificaContemSomenteLetras(String campo) {
+//        for(char palavra: campo.toCharArray()) {
+//            if(!(Character.isAlphabetic(palavra) || Character.isSpaceChar(palavra))) {
+//                throw new IllegalArgumentException("Só deve conter letras");
+//            }
+//        }
+//    }
+//
+//    private void verificaTamanhoAteCem(String campo) {
+//        if(campo.length() > 100) {
+//        throw new IllegalArgumentException("O campo deve ter até 100 caracteres");
+//        }
+//    }
     
     public LocalDate getDataNascimento() {
         return dataNascimento;
     }
 
     public void setDataNascimento(LocalDate dataNascimento) {
-        verificaDataNula(dataNascimento);
-        verificaDataNascimento(dataNascimento);
+//        verificaDataNula(dataNascimento);
+//        verificaDataNascimento(dataNascimento);
         this.dataNascimento = dataNascimento;
     }
 
-    private void verificaDataNascimento(LocalDate dataNascimento) {
-        if(dataNascimento.isBefore(LocalDate.of(1900, 1, 1)) || dataNascimento.isAfter(LocalDate.now())) {
-           throw new IllegalArgumentException("Coloque uma data a partir de 01/01/1900 e não ultrapasse a data de hoje"); 
-        }
-    }
-
-    private void verificaDataNula(LocalDate dataNascimento) {
-        if(dataNascimento == null) {
-            throw new IllegalArgumentException("O campo não deve ser nulo");
-        }
-    }
+//    private void verificaDataNascimento(LocalDate dataNascimento) {
+//        if(dataNascimento.isBefore(LocalDate.of(1900, 1, 1)) || dataNascimento.isAfter(LocalDate.now())) {
+//           throw new IllegalArgumentException("Coloque uma data a partir de 01/01/1900 e não ultrapasse a data de hoje"); 
+//        }
+//    }
+//
+//    private void verificaDataNula(LocalDate dataNascimento) {
+//        if(dataNascimento == null) {
+//            throw new IllegalArgumentException("O campo não deve ser nulo");
+//        }
+//    }
 
     public String getCpf() {
         return cpf;
     }
 
     public void setCpf(String cpf) {
-        verificaCampoNulo(cpf);
-        verificaTodosDigitosRepetidos(cpf);
-        verificaTamanhoCpf(cpf);
-        verificaContemSomenteNumeros(cpf);
-        ValidaCpf.validaCpf(cpf);
+//        verificaCampoNulo(cpf);
+//        verificaTodosDigitosRepetidos(cpf);
+//        verificaTamanhoCpf(cpf);
+//        verificaContemSomenteNumeros(cpf);
+//        ValidaCpf.validaCpf(cpf);
         this.cpf = cpf;
     }
     
-    private void verificaCampoNulo(String campo) {
-        if (campo == null) {
-            throw new IllegalArgumentException("Esse campo não pode ser nulo");
-        }
-    }
-    
-    private void verificaTamanhoCpf(String cpf) {
-        if(cpf.length() != 11) {
-            throw new IllegalArgumentException("O número do CPF deve ter 11 dígitos");
-        }
-    }
-    
-    private void verificaTodosDigitosRepetidos(String cpf) {
-        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") || cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555")|| cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf.equals("99999999999")) {
-            throw new IllegalArgumentException("Os digítos do CPF não podem ser todos repetidos"); 
-        }
-    }
+//    private void verificaCampoNulo(String campo) {
+//        if (campo == null) {
+//            throw new IllegalArgumentException("Esse campo não pode ser nulo");
+//        }
+//    }
+//    
+//    private void verificaTamanhoCpf(String cpf) {
+//        if(cpf.length() != 11) {
+//            throw new IllegalArgumentException("O número do CPF deve ter 11 dígitos");
+//        }
+//    }
+//    
+//    private void verificaTodosDigitosRepetidos(String cpf) {
+//        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") || cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555")|| cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf.equals("99999999999")) {
+//            throw new IllegalArgumentException("Os digítos do CPF não podem ser todos repetidos"); 
+//        }
+//    }
 
     public Endereco getEndereco() {
         return endereco;
     }
 
     public void setEndereco(Endereco endereco) {
-        verificaEnderecoNulo(endereco);
+//        verificaEnderecoNulo(endereco);
         this.endereco = endereco;
     }
     
-    private void verificaEnderecoNulo(Endereco endereco) {
-        if (endereco == null) {
-            throw new IllegalArgumentException("Esse campo não pode ser nulo");
-        }
-    }
+//    private void verificaEnderecoNulo(Endereco endereco) {
+//        if (endereco == null) {
+//            throw new IllegalArgumentException("Esse campo não pode ser nulo");
+//        }
+//    }
 
     public Contato getContato() {
         return contato;
     }
     
     public void setContato(Contato contato) {
-        verificaContatoNulo(contato);
+//        verificaContatoNulo(contato);
         this.contato = contato;
     }
     
-    private void verificaContatoNulo(Contato contato) {
-        if (contato == null) {
-            throw new IllegalArgumentException("Esse campo não pode ser nulo");
-        }
-    }
+//    private void verificaContatoNulo(Contato contato) {
+//        if (contato == null) {
+//            throw new IllegalArgumentException("Esse campo não pode ser nulo");
+//        }
+//    }
 
     @Override
     public int hashCode() {
